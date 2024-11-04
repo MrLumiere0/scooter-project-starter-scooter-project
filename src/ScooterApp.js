fcb1dd58378154a6322e6f8d490cb09e64a9b506
Hashid <hashidlouis@gmail.com>
2024-11-04 16:35:31 -0500

    Refactored existing codes and wrote sister tests

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -4,14 +4,13 @@ const Scooter = require('./Scooter')
 
 const Mario = new User("Mario", "pass", 30)
 
-
 class ScooterApp {
 
   constructor(){
     this.stations = {
-      Station1: [],
-      Station2: [],
-      Station3: []
+      "Station1": [],
+      "Station2": [],
+      "Station3": []
      }
     this.registeredUsers = { 
       "Mario":Mario
@@ -29,7 +28,6 @@ class ScooterApp {
         console.log("Too young to register")
        }
 
-      //  Check logic if this works after merge
        else if (age >= 18){
        const user = new User(username, password, age)
        return user
@@ -40,39 +38,49 @@ class ScooterApp {
   }
 
    loginUser(username, password) {  
-      if (this.registeredUsers.hasOwnProperty(username)) {
+    let user = this.registeredUsers[username]
+
+      if (user) {
         try {
-           username.login(password)
-          return ("User is logged in")
-  
+            user.login(password)
+            return ("User is logged in")
         }
 
-      catch {
+      catch (err) {
+        console.log(err)
        return ("Username or password is incorrect")
       }
   
     }
 
-    else if (!this.u.hasOwnProperty(username)) {
+    else if (!this.username.hasOwnProperty(username)) {
       return ("No such user is logged in")
     }
   }
 
+
+
    logoutUser(username) {
-    if (this.registeredUsers.hasOwnProperty(username)) {
-         username.logout()
-         console.log("User is logged out")
-  
+    let user = this.registeredUsers[username]
+    if (user) {
+      try {
+        user.logout()
+        return "User is logged out"
+      }
+
+      catch(err){
+        return err
+      }
     }
 
-    else if (!this.registeredUsers.hasOwnProperty(username)) {
-      console.log("No such user is logged in")
+    else if (!user) {
+     return("No such user is logged in")
     }
   }
 
   createScooter(station) {
+    let place = this.stations
 
-    //  Check logic if this works after merge
 
     if(this.stations.hasOwnProperty(station)) {
       const newScooter = new Scooter(station);
@@ -86,58 +94,41 @@ class ScooterApp {
   }
 }
 
-      //  Check logic if this works after merge
-  dockScooter (scooter, station){
-    if(this.stations.hasOwnProperty(station)){
-      if(this.stations[station].includes(scooter)){
-        console.log("Scooter already docked")
-      }
-    
 
-      else if(!this.stations[station].includes(scooter)){
-        this.stations.station.push(scooter)
-        scooter.station(station)
-        console.log("Created new Scooter ")  }
-  }
-
-
-    else if(!this.stations.hasOwnProperty(station)){
-        console.log("No such Station")
-    }
-  }
-
-        //  Check logic if this works after merge 
   rentScooter(scooter, user) {
-      let station = scooter.station
-      if (scooter.station == null) {
-        console.log("Scooter already rented")
-      }
+    let station = scooter.station
 
-      else if(this.stations.hasOwnProperty(station)){
+    if (station) {
+      return "Scooter already rented"
+    }
 
-        try{
-          station.filter(function (scooter) {
-            return scooter !== scooter 
-          })
-          scooter.user = user
-          console.log("scooter is")
-        }
+    else if(station == null){   
+        scooter.rent(user)
+        return "Scooter is rented"
+    }
 
-        catch{"error after station was validated"}
+}
+
+
+dockScooter (scooter, station){
+  let facility = scooter.station 
+
+  if(facility == station){
+      return "Scooter is docked"  }
+
+  else if((!this.stations.hasOwnProperty(station))){
+          return "No such station"
       }
   }
+  
+
 
   print(){
     console.log(this.registeredUsers)
     console.log(this.stations)
 }
-
- 
 }
 
 
-
-  // const New = new (ScooterApp)
-  // console.log(New.registerUser("Mario"))
 
 module.exports = ScooterApp
